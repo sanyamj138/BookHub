1bc252d644b954382f9cd88b5c87d02a946ffac9
Sanyam Jain <sanyamj138@gmail.com>
2023-06-29 15:28:49 +0530

    Queue Added

--- app/src/main/java/com/sanyamj138/bookhub/fragment/DashboardFragment.kt
+++ app/src/main/java/com/sanyamj138/bookhub/fragment/DashboardFragment.kt
@@ -27,31 +27,31 @@ class DashboardFragment : Fragment() {
     lateinit var layoutManager: RecyclerView.LayoutManager
     lateinit var btn_dashboard: Button
 
-    val bookList = arrayListOf(
-        "P.S. I love You",
-        "The Great Gatsby",
-        "Anna Karenina",
-        "Madame Bovary",
-        "War and Peace",
-        "Lolita",
-        "Middlemarch",
-        "The Adventures of Huckleberry Finn",
-        "Moby-Dick",
-        "The Lord of the Rings"
-    )
-
-    val bookInfoList = arrayListOf<Book>(
-        Book("P.S. I love You", "Cecelia Ahern", "Rs. 299", "4.5", R.drawable.ps_ily),
-        Book("The Great Gatsby", "F. Scott Fitzgerald", "Rs. 399", "4.1", R.drawable.great_gatsby),
-        Book("Anna Karenina", "Leo Tolstoy", "Rs. 199", "4.3", R.drawable.anna_kare),
-        Book("Madame Bovary", "Gustave Flaubert", "Rs. 500", "4.0", R.drawable.madame),
-        Book("War and Peace", "Leo Tolstoy", "Rs. 249", "4.8", R.drawable.war_and_peace),
-        Book("Lolita", "Vladimir Nabokov", "Rs. 349", "3.9", R.drawable.lolita),
-        Book("Middlemarch", "George Eliot", "Rs. 599", "4.2", R.drawable.middlemarch),
-        Book("The Adventures of Huckleberry Finn", "Mark Twain", "Rs. 699", "4.5", R.drawable.adventures_finn),
-        Book("Moby-Dick", "Herman Melville", "Rs. 499", "4.5", R.drawable.moby_dick),
-        Book("The Lord of the Rings", "J.R.R Tolkien", "Rs. 749", "5.0", R.drawable.lord_of_rings)
-    )
+//    val bookList = arrayListOf(
+//        "P.S. I love You",
+//        "The Great Gatsby",
+//        "Anna Karenina",
+//        "Madame Bovary",
+//        "War and Peace",
+//        "Lolita",
+//        "Middlemarch",
+//        "The Adventures of Huckleberry Finn",
+//        "Moby-Dick",
+//        "The Lord of the Rings"
+//    )
+//
+//    val bookInfoList = arrayListOf<Book>(
+//        Book("P.S. I love You", "Cecelia Ahern", "Rs. 299", "4.5", R.drawable.ps_ily),
+//        Book("The Great Gatsby", "F. Scott Fitzgerald", "Rs. 399", "4.1", R.drawable.great_gatsby),
+//        Book("Anna Karenina", "Leo Tolstoy", "Rs. 199", "4.3", R.drawable.anna_kare),
+//        Book("Madame Bovary", "Gustave Flaubert", "Rs. 500", "4.0", R.drawable.madame),
+//        Book("War and Peace", "Leo Tolstoy", "Rs. 249", "4.8", R.drawable.war_and_peace),
+//        Book("Lolita", "Vladimir Nabokov", "Rs. 349", "3.9", R.drawable.lolita),
+//        Book("Middlemarch", "George Eliot", "Rs. 599", "4.2", R.drawable.middlemarch),
+//        Book("The Adventures of Huckleberry Finn", "Mark Twain", "Rs. 699", "4.5", R.drawable.adventures_finn),
+//        Book("Moby-Dick", "Herman Melville", "Rs. 499", "4.5", R.drawable.moby_dick),
+//        Book("The Lord of the Rings", "J.R.R Tolkien", "Rs. 749", "5.0", R.drawable.lord_of_rings)
+//    )
 
 
     lateinit var recyclerAdapter: DashboardRecyclerAdapter
@@ -109,9 +109,9 @@ class DashboardFragment : Fragment() {
         val url = "http://13.235.250.119/vl/book/fetch_books/"
 
         val jsonObjectRequest = object :JsonObjectRequest(Method.GET, url, null, Response.Listener {
-
+            println("Response is $it")
         }, Response.ErrorListener {
-
+            println("Error is $it")
         }) {
             override fun getHeaders(): MutableMap<String, String> {
 
@@ -121,6 +121,8 @@ class DashboardFragment : Fragment() {
                 return headers
             }
         }
+
+        queue.add(jsonObjectRequest)
 
         return view
     }
--- app/src/main/java/com/sanyamj138/bookhub/model/Book.kt
+++ app/src/main/java/com/sanyamj138/bookhub/model/Book.kt
@@ -1,9 +1,10 @@
 package com.sanyamj138.bookhub.model
 
 data class Book (
+    val bookId: String,
     val bookName: String,
     val bookAuthor: String,
-    val bookPrice: String,
     val bookRating: String,
+    val bookPrice: String,
     val bookImage: Int
 )
